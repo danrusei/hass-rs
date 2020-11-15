b48a6724e3bdc8dd04ce7f7d62e8e71d6abb3b7d
Andrius Štikonas <andrius@stikonas.eu>
2020-11-15 17:55:10 +0000

    Make Entity attributes to be serde_json::Value.
    In Home Assistant there are custom attributes depending on entity.
    Having them stored as Value allows user to at least access them
    via attribute[attribute_name].
    
    Closes: https://github.com/danrusei/hass-rs/issues/2

--- src/types/entities.rs
+++ src/types/entities.rs
@@ -1,4 +1,5 @@
 use serde_derive::Deserialize;
+use serde_json::Value;
 
 /// This object represents the Home Assistant Entity
 ///
@@ -9,29 +10,8 @@ pub struct HassEntity {
     pub state: String,
     pub last_changed: String,
     pub last_updated: String,
-    pub attributes: HassEntityAttributeBase,
+    pub attributes: Value,
     pub context: Context,
-}
-
-///	This is part of HassEntity
-#[derive(Debug, Deserialize, PartialEq)]
-pub struct HassEntityAttributeBase {
-    #[serde(default)]
-    pub friendly_name: String,
-    #[serde(default)]
-    pub unit_of_measurement: String,
-    #[serde(default)]
-    pub icon: String,
-    #[serde(default)]
-    pub entity_picture: String,
-    #[serde(default)]
-    pub supported_features: u32,
-    #[serde(default)]
-    pub hidden: bool,
-    #[serde(default)]
-    pub assumed_state: bool,
-    #[serde(default)]
-    pub device_class: String,
 }
 
 /// General construct used by HassEntity and HassEvent
